c1b580fd9ec79a931e972b2dccabaab6d1647f78
lizebang <lizebang1998@gmaul.com>
2018-03-16 11:02:34 +0800

    add update script

---
+++ update.sh
@@ -0,0 +1,34 @@
+echo "rm"
+
+rm -rf .DS_Store
+rm -rf public
+
+echo "init"
+
+hugo
+cd public
+git init
+git remote add origin https://github.com/lizebang/lizebang.github.io
+git status
+
+echo "commit"
+
+git add .gitignore
+echo "*.DS_Store" >> .gitignore
+git add README.md
+echo "# lizebang.github.io\nMy Blog: [https://lizebang.github.io](https://lizebang.github.io)" >> README.md
+git add --all
+git stage *
+git commit -a -m "refresh"
+git status
+
+echo "gc"
+
+git gc
+
+echo "push"
+
+git push --force origin HEAD
+git status
+
+echo "done"
